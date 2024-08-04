0ac969b63885e42258f4294dee3a2aaebba27071
musraf-coding <musraf2107@gmail.com>
2024-08-04 13:45:26 +0530

    added

---
+++ src/Newpost.js
@@ -0,0 +1,36 @@
+
+
+const Newpost = ({handleSubmit,postName,setPostName,postBody,setPostBody}) => {
+  // const = useContext(DataContext)
+    return (
+    <main className='NewPost'>
+
+    <h2>New Contact</h2>
+    <form className='newPostForm' onSubmit={handleSubmit}>
+
+      <label htmlFor='postName'>Name:</label>
+      <input 
+          id='postName'
+          type='text'
+          placeholder='Name'
+          value={postName}
+          onChange={(e)=>setPostName(e.target.value)}
+      />
+        <label htmlFor='postBody'>Number:</label>
+      <input
+          id='postBody'
+          type='text'
+          placeholder='Number'
+          value={postBody}
+          onChange={(e)=>setPostBody(e.target.value)}
+      />
+
+      <button type='submit'>ADD</button>
+
+    </form>
+    </main>
+
+  )
+}
+
+export default Newpost
